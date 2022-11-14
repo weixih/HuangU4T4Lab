d4c4cc3aa13db202e16656d1437629dac0aea54a
student <@bths.edu>
2022-11-14 12:25:12 -0500

    eeeeee

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
-        String str1 = "apples and bananas";
-        while (str1.indexOf(" ") != -1) {
-            int index = str1.indexOf(" ");
-            str1 = str1.substring(0, index) + str1.substring(index + 1);
+    public static void main(String[] args){
+
+        int result = 0;
+        int m = 0;
+        while (m < 3) {
+            int k = 0;
+            while (k < 4) {
+                result++;
+                k++;
+                System.out.print("k: " + k);
+                System.out.println(", m: " + m);
+            }
+            m++;
         }
-        System.out.println(str1);
+        System.out.println("result: " + result);
+
+
+
     }
-}
+    }
